49606af7d7ba7b47effec6bcb9842d17ee948bab
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-06-25 18:26:56 -0300

    Att home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 {/* renders NoteList + button to create new note */}
 import './Home.css'
 import NoteList from '../../components/NoteList/NoteList'
+import NoteForm from '../../components/NoteForm/NoteForm'
+
 
 function Home() {
     return (
         <div className="home">
             <NoteList />
+            <NoteForm />
         </div>
     )
 }
