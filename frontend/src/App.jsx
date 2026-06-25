0fb749c1c6dff1a33c0b13e7304c6e5a77c23c0c
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-06-25 18:27:23 -0300

    Att app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
-import NoteList from './components/NoteList/NoteList'
 import Home from './pages/Home/Home'
 
 function App() {
- 
-
   return (
     <div className="app">
       <Header />
-      <NoteList />
       <Home />
-      </div>
+    </div>
   )
 }
 
