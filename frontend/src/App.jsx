ff79c1e0a2cdfa08409feae83eadeae70151a285
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-06-27 18:19:22 -0300

    Att app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
-import Header from './components/Header/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
