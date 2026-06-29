6de82adf0862d95a402b3a33e199554f890e7706
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-06-29 19:06:39 -0300

    Atualização url API

--- frontend/src/services/notaService.js
+++ frontend/src/services/notaService.js
@@ -3,22 +3,22 @@
 import api from './api'
 
 export async function buscarNotas() {
-    const response = await api.get('/notas/')
-    return response.data
+    const response = await api.get('/nota/')
+    return response.data.results  
 }
 
 export async function buscarNotasPorId(id) {
-    const response = await api.get(`/notas/${id}/`)
+    const response = await api.get(`/nota/${id}/`)
     return response.data
 }
 
 export async function criarNotas(dados) {
-    const response = await api.post('/notas/', dados)
+    const response = await api.post('/nota/', dados)
     return response.data
 }
 
 export async function atualizarNota(id, dados) {
-    const response = await api.put(`/notas/${id}/`, dados)
+    const response = await api.put(`/nota/${id}/`, dados)
     return response.data
 }
 
