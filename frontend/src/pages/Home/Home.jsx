66cf5f072861b358d5614297e26d82aa13a17fd1
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-07-01 18:43:32 -0300

    att home onselect e UseState

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import NoteList from '../../components/NoteList/NoteList'
 import NoteForm from '../../components/NoteForm/NoteForm'
 
 function Home() {
+    const [notaSelecionada, setNotaSelecionada] = useState(null)
+
     return (
         <div className="home">
             <Header />
             <div className="home-content">
-                <NoteList />
-                <NoteForm />
+                <NoteList onSelect={setNotaSelecionada} />
+                <NoteForm nota={notaSelecionada} />
             </div>
         </div>
     )
