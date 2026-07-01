c49deb9fe292a5e277fc21207ac5dad836244e7f
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-07-01 18:41:44 -0300

    Att NoteList add onclick

--- frontend/src/components/NoteList/NoteList.jsx
+++ frontend/src/components/NoteList/NoteList.jsx
@@ -3,19 +3,19 @@ import { buscarNotas } from '../../services/notaService'
 
 import './NoteList.css'
 
-function NoteList() {
+function NoteList({ onSelect }) {
     const [notas, setNotas] = useState([])
 
     useEffect(() => {
     buscarNotas().then(data => {
-        setNotas(data)  // saves the API response into 'notas'
+        setNotas(data)  
     })
 }, [])
 
-    return (
+        return (
         <section className="notelist">
             {notas.map((nota) => (
-                <div className="notelist-card" key={nota.id}>
+                <div className="notelist-card" key={nota.id} onClick={() => onSelect(nota)}>
                     <div className="notelist-titulo">{nota.titulo}</div>
                     <div className="notelist-conteudo">{nota.conteudo}</div>
                 </div>
